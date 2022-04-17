eb37ad42a97e2edb1a5e75bf06f6ce8a697fdd79
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-04-17 17:10:30 +0300

    partials added..

--- app.js
+++ app.js
@@ -9,16 +9,21 @@ const app=express();
 //*********template engine section**********
 app.set("view engine", "ejs");
 
-
-app.get('/',(req,res)=>{
-    res.status(200).render('index');
-})
-
 //*********middlewares section**********
 app.use(express.static('public'));
 
 //*********routes section**********
 
+app.get('/',(req,res)=>{
+    res.status(200).render('index',{
+        page_name:"index"
+    });
+});
+app.get('/about',(req,res)=>{
+    res.status(200).render('about',{
+        page_name:"about"
+    });
+})
 //*********Port section**********
 
 const port=3000;
