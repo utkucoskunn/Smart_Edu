3bfe1a34e9959c8a3307a41322b72e2e74af485f
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-04-19 11:55:02 +0300

    Personalized Content added!

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,7 @@ exports.loginUser = async (req, res) => {
                         if (same) {
                             //User Session
                             req.session.userID = user._id;
-                            res.status(200).redirect('/');
+                            res.status(200).redirect('/users/dashboard');
                         }
                     })
                 }
@@ -47,3 +47,11 @@ exports.logoutUser=(req,res)=>{
         res.redirect('/');
     });
 }
+
+exports.getDashboardPAge = async (req, res) => {
+    const user=await User.findOne({_id:req.session.userID})
+    res.status(200).render('dashboard', {
+        page_name: "dashboard",
+        user,
+    });
+};
