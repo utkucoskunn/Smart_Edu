22e46b483b29a36793dfd9a8fb1e50e3a5e2a93e
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-04-21 18:04:09 +0300

    Create Course Page added!

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,13 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const session=require('express-session');
+const Category=require('../models/Category')
 
 exports.createUser = async (req, res) => {
     try {
         const user = await User.create(req.body);
-        res.status(201).json({
-            status: 'succes',
-            user,
-        });
+        res.status(201).redirect('/login');
+
     } catch (error) {
         res.status(400).json({
             status: 'fail',
@@ -49,9 +48,11 @@ exports.logoutUser=(req,res)=>{
 }
 
 exports.getDashboardPAge = async (req, res) => {
-    const user=await User.findOne({_id:req.session.userID})
+    const user=await User.findOne({_id:req.session.userID});
+    const categories=await Category.find();
     res.status(200).render('dashboard', {
         page_name: "dashboard",
         user,
+        categories,
     });
 };
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -5,10 +5,8 @@ const Category = require('../models/Category');
 exports.createCourse = async (req, res) => {
     try {
         const course = await Course.create(req.body);
-        res.status(201).json({
-            status: 'succes',
-            course
-        });
+        res.status(201).redirect('/courses');
+
     } catch(error) {
         res.status(400).json({
             status: 'fail',
@@ -26,7 +24,7 @@ exports.getAllCourses = async (req, res) => {
         if (categorySlug){
             filter={category:category._id}
         }
-        const courses = await Course.find(filter);
+        const courses = await Course.find(filter).sort('-createdAt')
         const categories= await Category.find();
 
         res.status(200).render('courses',{
