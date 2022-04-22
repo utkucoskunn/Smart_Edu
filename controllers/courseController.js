3396b88c6b5c2d4cae27bcb6f7935859ab7a86b0
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-04-22 10:29:34 +0300

    Release Course  added!

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -49,10 +49,12 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
     try {
+        const user=await User.findById(req.session.userID);
         const course = await Course.findOne({slug: req.params.slug}).populate('user')
         res.status(200).render('course', {
             course,
             page_name: 'courses',
+            user
         });
 
     } catch (error) {
@@ -67,6 +69,22 @@ exports.enrollCourse = async (req, res) => {
     try {
         const user=await User.findById(req.session.userID);
         await user.courses.push({_id:req.body.course_id});
+        await user.save();
+        res.status(200).redirect('/users/dashboard');
+
+
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
+        });
+    }
+};
+
+exports.releaseCourse = async (req, res) => {
+    try {
+        const user=await User.findById(req.session.userID);
+        await user.courses.pull({_id:req.body.course_id});
         await user.save();
         res.status(200).redirect('/users/dashboard');
 
