9f3b0d041cb8b7bb18ca564778d190bba4f46316
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-04-24 12:02:22 +0300

    mailstrap  added!

--- controllers/pagecontroller.js
+++ controllers/pagecontroller.js
@@ -1,4 +1,6 @@
-exports.getIndexPage=(req,res)=> {
+const nodemailer = require('nodemailer');
+
+exports.getIndexPage = (req, res) => {
     console.log(req.session.userID);
     res.status(200).render('index', {
         page_name: "index",
@@ -20,4 +22,53 @@ exports.getLoginPage = (req, res) => {
     res.status(200).render('login', {
         page_name: "login",
     });
+};
+
+exports.getContactPage = (req, res) => {
+    res.status(200).render('contact', {
+        page_name: "contact",
+    });
+};
+
+exports.sendMail = async (req, res) => {
+    try {
+        const outputMessage = `
+    <h1>Mail Details</h1>
+    <ul>
+    <li>Name: ${req.body.name} </li>
+    <li>Email: ${req.body.email} </li>
+</ul>
+<h1>Message</h1>
+<p>${req.body.message}</p>
+    `
+        let transporter = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "666886797b5c17", // gmail account
+                pass:  "d86d57bec43199", // gmail password
+            }
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: ' "Smart EDU Contact Form" <smtp.mailtrap.io>', // sender address
+            to: "utkucoskun@windowslive.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            html: outputMessage, // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+
+        res.status(200).redirect('contact');
+    } catch (err) {
+        console.log(err);
+        res.status(200).redirect('contact');
+    }
 };
