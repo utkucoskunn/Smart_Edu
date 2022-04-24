9b0762c3e3b09fc851e8640d1802da9291bd74d0
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-04-24 07:27:37 +0300

    Search Course  added!

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,6 +1,6 @@
 const Course = require('../models/Course');
 const Category = require('../models/Category');
-const User=require('../models/User');
+const User = require('../models/User');
 
 
 exports.createCourse = async (req, res) => {
@@ -25,12 +25,27 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
     try {
         const categorySlug = req.query.categories;
+        const query = req.query.search;
+
         const category = await Category.findOne({slug: categorySlug});
         let filter = {};
         if (categorySlug) {
             filter = {category: category._id}
         }
-        const courses = await Course.find(filter).sort('-createdAt')
+        if (query) {
+            filter = {name: query}
+        }
+
+        if (!query && !categorySlug) {
+            filter.name = "",
+                filter.category = null
+        }
+        const courses = await Course.find({
+            $or:[
+                {name: { $regex:'.*' + filter.name + '.*', $options: 'i'}},
+                {category: filter.category}
+            ]
+        }).sort('-createdAt').populate('user');
         const categories = await Category.find();
 
         res.status(200).render('courses', {
@@ -49,12 +64,14 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
     try {
-        const user=await User.findById(req.session.userID);
-        const course = await Course.findOne({slug: req.params.slug}).populate('user')
+        const user = await User.findById(req.session.userID);
+        const course = await Course.findOne({slug: req.params.slug}).populate('user');
+        const categories = await Category.find();
         res.status(200).render('course', {
             course,
             page_name: 'courses',
-            user
+            user,
+            categories
         });
 
     } catch (error) {
@@ -67,8 +84,8 @@ exports.getCourse = async (req, res) => {
 
 exports.enrollCourse = async (req, res) => {
     try {
-        const user=await User.findById(req.session.userID);
-        await user.courses.push({_id:req.body.course_id});
+        const user = await User.findById(req.session.userID);
+        await user.courses.push({_id: req.body.course_id});
         await user.save();
         res.status(200).redirect('/users/dashboard');
 
@@ -83,8 +100,8 @@ exports.enrollCourse = async (req, res) => {
 
 exports.releaseCourse = async (req, res) => {
     try {
-        const user=await User.findById(req.session.userID);
-        await user.courses.pull({_id:req.body.course_id});
+        const user = await User.findById(req.session.userID);
+        await user.courses.pull({_id: req.body.course_id});
         await user.save();
         res.status(200).redirect('/users/dashboard');
 
